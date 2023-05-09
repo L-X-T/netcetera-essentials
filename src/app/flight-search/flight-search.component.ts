L-X-T/ssi-essentials

Add extra observavle/subscription/async demo

src/app/flight-search/flight-search.component.ts
@@ -1,34 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { Flight } from '../entities/flight';
 import { FlightService } from './flight.service';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css']
 })
-export class FlightSearchComponent implements OnInit {
+export class FlightSearchComponent implements OnInit, OnDestroy {
   from = 'Graz';
   to = 'Hamburg';
+
   flights: Flight[] = [];
+  flights$: Observable<Flight[]>;
+  flightsSubscription: Subscription;
+
   selectedFlight: Flight;
 
   message: string;
+
+  onDestroySubject = new Subject<void>();
 
   constructor(private flightService: FlightService) {}
 
   ngOnInit(): void {}
 
   search(): void {
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
-        this.flights = flights;
-      },
-      error: (errResp) => {
-        console.error('Error loading flights', errResp);
-      }
-    });
+    // 1. my observable
+    this.flights$ = this.flightService.find(this.from, this.to);
+
+    // 2. my observer
+    const flightsObserver: Observer<Flight[]> = {
+      next: (flights) => (this.flights = flights),
+      error: (errResp) => console.error('Error loading flights', errResp),
+      complete: () => console.debug('complete')
+    };
+
+    // 3. my subscription
+    // this.flightsSubscription = this.flights$.subscribe(flightsObserver);
+
+    this.flights$.pipe(takeUntil(this.onDestroySubject)).subscribe(flightsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // 4. my unsubscribe
+    // this.flightsSubscription?.unsubscribe();
+
+    // const my$ = this.onDestroySubject.asObservable();
+    this.onDestroySubject.next();
+    this.onDestroySubject.complete();
   }
 
   select(f: Flight): void {

L-X-T/netcetera-essentials

src/app/flight-search/flight-search.component.ts
@@ -1,35 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 import { Flight } from '../entities/flight';
 import { FlightService } from './flight.service';
+import { Observable, Observer, Subject, Subscription } from 'rxjs';
+import { share, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.css']
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnDestroy {
   from = 'Graz';
   to = 'Hamburg';
+
   flights: Flight[] = [];
+  flights$: Observable<Flight[]> | undefined;
+  flightsSubscription: Subscription | undefined;
+
   selectedFlight: Flight | undefined | null;
 
   message = '';
 
+  onDestroySubject = new Subject<void>();
+
   constructor(private flightService: FlightService) {}
 
   search(): void {
-    this.flightService.find(this.from, this.to).subscribe({
-      next: (flights) => {
-        this.flights = flights;
-      },
-      error: (errResp) => {
-        console.error('Error loading flights', errResp);
-      },
-      complete: () => {
-        console.warn('complete');
-      }
-    });
+    // 1. my observable
+    this.flights$ = this.flightService.find(this.from, this.to).pipe(share());
+
+    // 2. my observer
+    const flightsObserver: Observer<Flight[]> = {
+      next: (flights) => (this.flights = flights),
+      error: (errResp) => console.error('Error loading flights', errResp),
+      complete: () => console.warn('complete')
+    };
+
+    // 3. my subscription
+    // this.flightsSubscription = this.flights$.subscribe(flightsObserver);
+
+    this.flights$.pipe(takeUntil(this.onDestroySubject)).subscribe(flightsObserver);
+  }
+
+  ngOnDestroy(): void {
+    // 4. my unsubscribe
+    // this.flightsSubscription?.unsubscribe();
+
+    // const my$ = this.onDestroySubject.asObservable();
+    this.onDestroySubject.next(void 0);
+    this.onDestroySubject.complete();
   }
 
   select(f: Flight): void {