c1c00bd587fd0b11d8f38d34ca68e87099628275
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-05-11 14:07:38 +0200

    [Bonus]: Using feature selectors

--- src/app/flight-booking/+state/flight-booking.selectors.ts
+++ src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -3,10 +3,8 @@ import * as fromFlightBooking from './flight-booking.reducer';
 
 export const selectFlightBookingState = createFeatureSelector<fromFlightBooking.State>(fromFlightBooking.flightBookingFeatureKey);
 
-export const selectFlights = (appState: fromFlightBooking.FlightBookingAppState) =>
-  appState[fromFlightBooking.flightBookingFeatureKey].flights;
-export const negativeList = (appState: fromFlightBooking.FlightBookingAppState) =>
-  appState[fromFlightBooking.flightBookingFeatureKey].negativeList;
+export const selectFlights = createSelector(selectFlightBookingState, (featureState) => featureState.flights);
+export const negativeList = createSelector(selectFlightBookingState, (featureState) => featureState.negativeList);
 
 export const selectedFilteredFlights = createSelector(selectFlights, negativeList, (flights, negativeList) =>
   flights.filter((f) => !negativeList.includes(f.id))
